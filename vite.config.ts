dd009f4abec5b9e7d494fbb84233783ddac72441
deffi <deffi.charlina@eurokars.co.id>
2025-11-21 09:22:33 +0700

    fix: :bug: Rebuild vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'antd': ['antd'],
+          'mediapipe': ['@mediapipe/hands', '@mediapipe/camera_utils'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  }
 })
